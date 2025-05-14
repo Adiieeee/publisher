cc5401069366f14ceafa6fe28fbd62a861f31217
Adiieeee <adiansyah.nugraha@gmail.com>
2025-05-14 10:47:15 +0700

    Initial Publisher code.

---
+++ target/debug/deps/publisher.d
@@ -0,0 +1,5 @@
+D:\Universitas\Semester\Semester 4\Adpro\Module\tutorial8\publisher\target\debug\deps\publisher.exe: src\main.rs
+
+D:\Universitas\Semester\Semester 4\Adpro\Module\tutorial8\publisher\target\debug\deps\publisher.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/publisher.d
@@ -0,0 +1,1 @@
+D:\Universitas\Semester\Semester\ 4\Adpro\Module\tutorial8\publisher\target\debug\publisher.exe: D:\Universitas\Semester\Semester\ 4\Adpro\Module\tutorial8\publisher\src\main.rs
